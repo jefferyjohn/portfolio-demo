67432c4490ddd45c711bec6c507af8de0ad1a2d5
Jeffery John <jefferyjohn2000@gmail.com>
2023-12-09 15:28:27 +0000

    stable

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 import Head from 'next/head';
-import Image from 'next/image'
+import Image from "next/legacy/image"
 import { useEffect } from 'react';
 import { clarity } from 'react-microsoft-clarity';
 import "react-multi-carousel/lib/styles.css";
+import '../public/main.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faYoutube, faGithub, faInstagram, faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+import { InlineWidget } from 'react-calendly';
 
 export default function Home() {
 
@@ -14,54 +20,15 @@ export default function Home() {
   }
     , []);
 
-  useEffect(() => {
-    // Function to initialize Calendly script
-    const initializeCalendlyScript = () => {
-      // Check if Calendly is defined
-      if ((window as any).Calendly !== 'undefined') {
-        // Initialize Calendly inline widget
-        (window as any).Calendly.initInlineWidget({
-          url: 'https://calendly.com/jefferyjohn/45-minute-meeting?hide_event_type_details=1&hide_gdpr_banner=1',
-        });
-      } else {
-        // Retry initialization after a delay
-        setTimeout(initializeCalendlyScript, 1000);
-      }
-    };
-
-    // Load Calendly script only if it hasn't been loaded before
-    if (!document.querySelector('script[src="https://assets.calendly.com/assets/external/widget.js"]')) {
-      const script = document.createElement('script');
-      script.src = 'https://assets.calendly.com/assets/external/widget.js';
-      script.async = true;
-      script.onload = initializeCalendlyScript;
-      document.head.appendChild(script);
-    } else {
-      initializeCalendlyScript();
-    }
-
-    // Cleanup function to remove the script when the component unmounts
-    return () => {
-      const existingScript = document.querySelector('script[src="https://assets.calendly.com/assets/external/widget.js"]');
-      if (existingScript) {
-        existingScript.remove();
-      }
-    };
-  }, []);
-
   return (
     <main>
       <Head>
-        {/* Calendly Widget Script */}
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async
-        />
+        <script src="https://kit.fontawesome.com/cafed16778.js" crossOrigin="anonymous"></script>
       </Head>
 
 
       <script
+        // As a limitation of the NextJS, this is the only reliable way for the analytics script to be loaded
         dangerouslySetInnerHTML={
           {
             __html: `
@@ -77,157 +44,165 @@ export default function Home() {
           }}
       />
       <header className="flex justify-end items-start">
-        <div className="nav">
-          <ul className="flex space-x-4">
-            <li><a href="#hero">Home</a></li>
-            <li><a href="https://docs.google.com/document/d/1P_OGRLE3YFpMaA_HrIfT9GhvJX6ZVo1uLXGaagscEcE/export?format=pdf">Resume</a></li>
-            <li><a href="https://docs.google.com/document/d/1_hXpiQh4v_hvvW-0ZLLrTg2pLDjdtK_U/edit?usp=sharing&ouid=103091850561061166757&rtpof=true&sd=true">98-019</a></li>
-            <li><a href="#media">Media</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+        <div className="nav shadow-lg ">
+          <ul className="flex space-x-4 pt-5 pr-16 shadow-xl rounded-full p-3">
+            <li><a href="#home" className="text-xl">Home</a></li>
+            <li><a href="https://docs.google.com/document/d/1P_OGRLE3YFpMaA_HrIfT9GhvJX6ZVo1uLXGaagscEcE/export?format=pdf" className="text-xl">Resume</a></li>
+            <li><a href="https://docs.google.com/document/d/1_hXpiQh4v_hvvW-0ZLLrTg2pLDjdtK_U/edit?usp=sharing&ouid=103091850561061166757&rtpof=true&sd=true" className="text-xl">98-019</a></li>
+            <li><a href="#media" className="text-xl">Media</a></li>
+            <li><a href="#projects" className="text-xl">Projects</a></li>
+            <li><a href="#contact" className="text-xl">Contact</a></li>
           </ul>
         </div>
       </header>
 
-      <section className="hero relative">
+      <section id="home" className="hero relative" >
         <div className="hero-container relative">
           {/* Image */}
-          <div className="hero-image relative">
-            <Image src="/hero.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
+          <div className="hero-image relative h-screen">
+            <Image src="/hero.png" alt="Jeffery John" layout="fill"
+              className="object-cover w-full h-full" priority={true} />
           </div>
 
           {/* Text */}
           <div className="hero-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-            <h1 className="text-6xl font-bold">Jeffery John</h1>
-            <h2 className="text-2xl">Developer & Designer</h2>
-            <p className="text-xl">Carnegie Mellon University</p>
-          </div>
-        </div>
-      </section>
-      <section className="media">
-        <h1 className="text-6xl font-bold text-center">Media</h1>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-1.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://www.instagram.com/p/B1WW17PpF61/" className="block h-48">
-              <Image src="/test-2.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-3.jpg" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-4.jpg" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-5.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-6.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-7.jpg" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-8.jpg" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-          <div className="col">
-            <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="block h-48">
-              <Image src="/test-9.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
-            </a>
-          </div>
-        </div>
-      </section>
-
-
-      <section className="projects">
-        <h1 className="text-6xl font-bold text-center">Projects</h1>
-
-        <div className="carousel-container overflow-hidden relative">
-          <div className="carousel-wrapper flex animate-slide" id="carousel">
-            {/* First carousel item */}
-            <div className="carousel-item">
-              <a href="https://blink-weld.vercel.app/" className="">
-                <Image src="/c1.png" alt="Jeffery John" width={300} height={300} className="object-cover w-full h-full" />
+            <h1 className="text-8xl font-bold">JEFFERY JOHN</h1>
+            <div className="socials flex justify-center space-x-4 mt-4">
+              <a href="https://www.linkedin.com/in/jefferyjohn/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
+              </a>
+              <a href="https://www.youtube.com/@jefferyjohn" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faYoutube} size="3x" />
+              </a>
+              <a href="https://github.com/jefferyjohn" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="3x" />
+              </a>
+              <a href="https://www.instagram.com/jefferyjohn_/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} size="3x" />
+              </a>
+              <a href="https://open.spotify.com/user/themagicalwarlock" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faSpotify} size="3x" />
+              </a>
+              <a href="mailto:jefferyjohn@cmu.edu" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faEnvelope} size="3x" />
               </a>
             </div>
+            <div id="about" className="pt-16">
+              <h3 id="description" className="text-2xl">Hi! I'm a developer, maker, and hacker @ Carnegie Mellon University.</h3>
+              {/* Animated Arrows */}
+              <div className="flex justify-center space-x-4 mt-4 pt-36 animate-bounce">
+                <a href="#media">
+                  <FontAwesomeIcon icon={faArrowDown} size="2x" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div id="content">
+        <section id="media" className="media w-9/10 pt-12">
+          <h1 className="text-6xl font-bold text-center">Media</h1>
+          <div className="flex flex-col items-center">
+            <section id="news" className="news w-9/10 pt-12">
+              <div className="grid grid-cols-3 gap-4 gap-y-8 mt-4">
+                <div className="news-item">
+                  <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="">
+                    <Image src="/media/mlh-top50.png" alt="Major League Hacking Top 50" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div> 
+                <div className="news-item">
+                  <a href="https://www.redandblack.com/uganews/uga-s-small-satellite-research-lab-looks-to-the-stars-for-solutions-on-earth/article_0d71f854-36d3-11ec-8210-6f0ff0ab47f6.html" className="">
+                    <Image src="/media/small-sat.png" alt="UGA Small Satellite Research Lab" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://news.uga.edu/traveling-mars-complicated-teamwork/" className="">
+                    <Image src="/media/fusion.png" alt="UGA Project FUSION" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://news.uga.edu/student-tour-of-georgia-2020/" className="">
+                    <Image src="/media/uga-tour.png" alt="Student Tour of Georgia" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://www.linkedin.com/pulse/acm-uga-2021-2022-uga-acm" className="">
+                    <Image src="/media/uga-acm.png" alt="ACM @ UGA 2021-2022 Recap" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://picoctf.org/about.html" className="">
+                    <Image src="/media/picoctf.png" alt="picoCTF Overview" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://www.acmatcmu.com/hackcmu/" className="">
+                    <Image src="/media/hackcmu.png" alt="ACM@CMU HackCMU Overview" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://www.forsythnews.com/local/lifestyles/club-at-pinecrest-combines-crafting-with-service/" className="">
+                    <Image src="/media/crafters-club.png" alt="Crafter's Club Pinecrest Academy" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+                <div className="news-item">
+                  <a href="https://www.instagram.com/p/B1WW17PpF61/" className="">
+                    <Image src="/media/sanford-print.png" alt="UGA 3D Printing of Sanford Stadium" width={400} height={400} className="object-cover w-full" />
+                  </a>
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
 
-            {/* Second carousel item */}
-            <div className="carousel-item">
-              <a href="https://jefferyjohn.github.io/poke/" className="">
-                <Image src="/c2.png" alt="Image 2" width={300} height={300} className="object-cover w-full h-full" />
-              </a>
+        <section id="projects" className="projects w-9/10 pt-20" >
+          <h1 className="text-6xl font-bold text-center">Projects</h1>
+
+          <div className="carousel-container overflow-hidden relative pt-24">
+            <div className="carousel-wrapper flex animate-slide" id="carousel">
+              <div className="carousel-item">
+                <a href="https://blink-weld.vercel.app/" className="">
+                  <Image src="/projects/blink.png" alt="Retrieval Augmented Generation Chat App" width={700} height={700} className="object-cover w-1/3 " />
+                </a>
+              </div>
+              <div className="carousel-item">
+                <a href="https://jefferyjohn.github.io/poke/" className="">
+                  <Image src="/projects/poke.png" alt="Physics based simulator with drawing" width={700} height={700} className="object-cover w-1/3 " />
+                </a>
+              </div>
+              <div className="carousel-item">
+                <a href="https://irislunarrover.space/" className="">
+                  <Image src="/projects/iris-moonyard.png" alt="MoonYard for Iris" width={700} height={700} className="object-cover w-1/3 " />
+                </a>
+              </div>
+              <div className="carousel-item">
+                <a href="https://redrobot.roboclub.org/" className="">
+                  <Image src="/projects/red-robot.png" alt="Robotics Club Red Robot" width={700} height={700} className="object-cover w-1/3 " />
+                </a>
+              </div>
+              <div className="carousel-item">
+                <a href="https://www.acmatcmu.com/hackcmu/" className="">
+                  <Image src="/projects/hackcmu.png" alt="ACM@CMU HackCMU" width={700} height={700} className="object-cover w-1/3 " />
+                </a>
+              </div>
+              <div className="carousel-item">
+                <a href="https://cmrocketcommand.wordpress.com/" className="">
+                  <Image src="/projects/rocket-club.png" alt="Carnegie Mellon Rocket Command" width={700} height={700} className="object-cover w-1/3 " />
+                </a>
+              </div>
             </div>
 
-            {/* Third carousel item */}
-            <div className="carousel-item">
-              <a href="https://irislunarrover.space/" className="">
-                <Image src="/c3.png" alt="Image 3" width={300} height={300} className="object-cover w-full h-full" />
-              </a>
-            </div>
-
-            {/* Add more carousel items as needed */}
           </div>
 
-          <style jsx>{`
-    @keyframes slide {
-      0% {
-        transform: translateX(0);
-      }
-      100% {
-        transform: translateX(-100%);
-      }
-    }
-
-    .animate-slide {
-      animation: slide 15s linear infinite;
-    }
-
-    .animate-slide:hover {
-      animation-play-state: paused;
-    }
-
-    .carousel-wrapper {
-      display: flex; /* Ensure all carousel items are in a row */
-      width: 300%; /* Set the width based on the number of slides */
-    }
-
-    .carousel-item {
-      flex: 0 0 33.33%; /* Each carousel item takes up 1/3 of the container width */
-    }
-  `}</style>
-        </div>
-
-      </section>
-      <section className="contact">
-        <h1 className="text-6xl font-bold text-center">Contact</h1>
-
-        <div className="calendly-container">
-          <div className="calendly-inline-widget" style={{ minWidth: '320px', height: '700px' }} />
-        </div>
-
-        <style jsx>{`
-        .calendly-container {
-          overflow: hidden;
-        }
-      `}</style>
-      </section>
+        </section>
+        <section id="contact" className="contact w-9/10 pt-20">
+          <h1 className="text-6xl font-bold text-center">Contact</h1>
+          <div className="calendly-container" >
+            {/* As a limitation of the API, no class or id declarations are allowed here - direct style tag is unavoidable */}
+            <InlineWidget url="https://calendly.com/jefferyjohn/45-minute-meeting" styles={{ height: '1000px' }} />
+          </div>
+        </section>
+      </div>
     </main >
   )
 }
