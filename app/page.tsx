6e680f534568a4e6119ad1cbf43dae06c5f04602
Jeffery John <jefferyjohn2000@gmail.com>
2023-12-09 16:49:18 +0000

    add mobile support

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,8 @@ export default function Home() {
 
 
       <header className="flex justify-end items-start">
-        <div className="nav shadow-lg">
-          <ul className="flex space-x-4 pt-5 pr-16 shadow-xl rounded-full p-3">
+        <div className="nav">
+          <ul id="nav-links" className="flex space-x-4 pt-5 shadow-xl rounded-full p-3">
             <li><a href="#home" className="text-xl">Home</a></li>
             <li><a href="https://docs.google.com/document/d/1P_OGRLE3YFpMaA_HrIfT9GhvJX6ZVo1uLXGaagscEcE/export?format=pdf" className="text-xl">Resume</a></li>
             <li><a href="https://docs.google.com/document/d/1_hXpiQh4v_hvvW-0ZLLrTg2pLDjdtK_U/edit?usp=sharing&ouid=103091850561061166757&rtpof=true&sd=true" className="text-xl">98-019</a></li>
@@ -52,30 +52,30 @@ export default function Home() {
         <div className="hero-container relative">
           {/* Image */}
           <div className="hero-image relative h-screen">
-            <Image src="/hero.png" alt="Jeffery John" layout="fill"
+            <Image src="/hero.png" alt="SPOC satellite" layout="fill"
               className="object-cover w-full h-full" priority={true} />
           </div>
 
           {/* Text */}
           <div className="hero-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-            <h1 className="text-8xl font-bold">JEFFERY JOHN</h1>
+            <h1 id="name" className="text-8xl font-bold">JEFFERY JOHN</h1>
             <div className="socials flex justify-center space-x-4 mt-4">
-              <a href="https://www.linkedin.com/in/jefferyjohn/" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.linkedin.com/in/jefferyjohn/" target="_blank" rel="noopener noreferrer" className="social">
                 <FontAwesomeIcon icon={faLinkedin} size="3x" />
               </a>
-              <a href="https://www.youtube.com/@jefferyjohn" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.youtube.com/@jefferyjohn" target="_blank" rel="noopener noreferrer" className="social">
                 <FontAwesomeIcon icon={faYoutube} size="3x" />
               </a>
-              <a href="https://github.com/jefferyjohn" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/jefferyjohn" target="_blank" rel="noopener noreferrer" className="social">
                 <FontAwesomeIcon icon={faGithub} size="3x" />
               </a>
-              <a href="https://www.instagram.com/jefferyjohn_/" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.instagram.com/jefferyjohn_/" target="_blank" rel="noopener noreferrer" className="social">
                 <FontAwesomeIcon icon={faInstagram} size="3x" />
               </a>
-              <a href="https://open.spotify.com/user/themagicalwarlock" target="_blank" rel="noopener noreferrer">
+              <a href="https://open.spotify.com/user/themagicalwarlock" target="_blank" rel="noopener noreferrer" className="social">
                 <FontAwesomeIcon icon={faSpotify} size="3x" />
               </a>
-              <a href="mailto:jefferyjohn@cmu.edu" target="_blank" rel="noopener noreferrer">
+              <a href="mailto:jefferyjohn@cmu.edu" target="_blank" rel="noopener noreferrer" className="social">
                 <FontAwesomeIcon icon={faEnvelope} size="3x" />
               </a>
             </div>
@@ -98,47 +98,47 @@ export default function Home() {
             <section id="news" className="news w-9/10 pt-12">
               <div className="grid grid-cols-3 gap-4 gap-y-8 mt-4">
                 <div className="news-item">
-                  <a href="https://top.mlh.io/2022/profiles/jeffery-john" className="">
+                  <a href="https://top.mlh.io/2022/profiles/jeffery-john" >
                     <Image src="/media/mlh-top50.png" alt="Major League Hacking Top 50" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://www.redandblack.com/uganews/uga-s-small-satellite-research-lab-looks-to-the-stars-for-solutions-on-earth/article_0d71f854-36d3-11ec-8210-6f0ff0ab47f6.html" className="">
+                  <a href="https://www.redandblack.com/uganews/uga-s-small-satellite-research-lab-looks-to-the-stars-for-solutions-on-earth/article_0d71f854-36d3-11ec-8210-6f0ff0ab47f6.html" >
                     <Image src="/media/small-sat.png" alt="UGA Small Satellite Research Lab" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://news.uga.edu/traveling-mars-complicated-teamwork/" className="">
+                  <a href="https://news.uga.edu/traveling-mars-complicated-teamwork/" >
                     <Image src="/media/fusion.png" alt="UGA Project FUSION" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://news.uga.edu/student-tour-of-georgia-2020/" className="">
+                  <a href="https://news.uga.edu/student-tour-of-georgia-2020/" >
                     <Image src="/media/uga-tour.png" alt="Student Tour of Georgia" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://www.linkedin.com/pulse/acm-uga-2021-2022-uga-acm" className="">
+                  <a href="https://www.linkedin.com/pulse/acm-uga-2021-2022-uga-acm" >
                     <Image src="/media/uga-acm.png" alt="ACM @ UGA 2021-2022 Recap" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://picoctf.org/about.html" className="">
+                  <a href="https://picoctf.org/about.html" >
                     <Image src="/media/picoctf.png" alt="picoCTF Overview" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://www.acmatcmu.com/hackcmu/" className="">
+                  <a href="https://www.acmatcmu.com/hackcmu/" >
                     <Image src="/media/hackcmu.png" alt="ACM@CMU HackCMU Overview" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://www.forsythnews.com/local/lifestyles/club-at-pinecrest-combines-crafting-with-service/" className="">
+                  <a href="https://www.forsythnews.com/local/lifestyles/club-at-pinecrest-combines-crafting-with-service/" >
                     <Image src="/media/crafters-club.png" alt="Crafter's Club Pinecrest Academy" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
                 <div className="news-item">
-                  <a href="https://www.instagram.com/p/B1WW17PpF61/" className="">
+                  <a href="https://www.instagram.com/p/B1WW17PpF61/" >
                     <Image src="/media/sanford-print.png" alt="UGA 3D Printing of Sanford Stadium" width={400} height={400} className="object-cover w-full" />
                   </a>
                 </div>
@@ -153,64 +153,64 @@ export default function Home() {
           <div className="carousel-container overflow-hidden relative pt-24">
             <div className="carousel-wrapper flex animate-slide" id="carousel">
               <div className="carousel-item">
-                <a href="https://blink-weld.vercel.app/" className="">
-                  <Image src="/projects/blink.png" alt="Retrieval Augmented Generation Chat App" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://blink-weld.vercel.app/" >
+                  <Image src="/projects/blink.png" alt="Retrieval Augmented Generation Chat App" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://jefferyjohn.github.io/poke/" className="">
-                  <Image src="/projects/poke.png" alt="Physics based simulator with drawing" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://jefferyjohn.github.io/poke/" >
+                  <Image src="/projects/poke.png" alt="Physics based simulator with drawing" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://irislunarrover.space/" className="">
-                  <Image src="/projects/iris-moonyard.png" alt="MoonYard for Iris" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://irislunarrover.space/" >
+                  <Image src="/projects/iris-moonyard.png" alt="MoonYard for Iris" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://redrobot.roboclub.org/" className="">
-                  <Image src="/projects/red-robot.png" alt="Robotics Club Red Robot" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://redrobot.roboclub.org/" >
+                  <Image src="/projects/red-robot.png" alt="Robotics Club Red Robot" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://www.acmatcmu.com/hackcmu/" className="">
-                  <Image src="/projects/hackcmu.png" alt="ACM@CMU HackCMU" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://www.acmatcmu.com/hackcmu/" >
+                  <Image src="/projects/hackcmu.png" alt="ACM@CMU HackCMU" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://cmrocketcommand.wordpress.com/" className="">
-                  <Image src="/projects/rocket-club.png" alt="Carnegie Mellon Rocket Command" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://cmrocketcommand.wordpress.com/" >
+                  <Image src="/projects/rocket-club.png" alt="Carnegie Mellon Rocket Command" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               {/* Duplicate slides so that it's a seamless loop */}
               <div className="carousel-item">
-                <a href="https://blink-weld.vercel.app/" className="">
-                  <Image src="/projects/blink.png" alt="Retrieval Augmented Generation Chat App" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://blink-weld.vercel.app/" >
+                  <Image src="/projects/blink.png" alt="Retrieval Augmented Generation Chat App" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://jefferyjohn.github.io/poke/" className="">
-                  <Image src="/projects/poke.png" alt="Physics based simulator with drawing" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://jefferyjohn.github.io/poke/" >
+                  <Image src="/projects/poke.png" alt="Physics based simulator with drawing" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://irislunarrover.space/" className="">
-                  <Image src="/projects/iris-moonyard.png" alt="MoonYard for Iris" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://irislunarrover.space/" >
+                  <Image src="/projects/iris-moonyard.png" alt="MoonYard for Iris" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://redrobot.roboclub.org/" className="">
-                  <Image src="/projects/red-robot.png" alt="Robotics Club Red Robot" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://redrobot.roboclub.org/" >
+                  <Image src="/projects/red-robot.png" alt="Robotics Club Red Robot" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://www.acmatcmu.com/hackcmu/" className="">
-                  <Image src="/projects/hackcmu.png" alt="ACM@CMU HackCMU" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://www.acmatcmu.com/hackcmu/" >
+                  <Image src="/projects/hackcmu.png" alt="ACM@CMU HackCMU" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
               <div className="carousel-item">
-                <a href="https://cmrocketcommand.wordpress.com/" className="">
-                  <Image src="/projects/rocket-club.png" alt="Carnegie Mellon Rocket Command" width={700} height={700} className="object-cover w-1/3 " />
+                <a href="https://cmrocketcommand.wordpress.com/" >
+                  <Image src="/projects/rocket-club.png" alt="Carnegie Mellon Rocket Command" width={700} height={700} className="object-cover w-1/3 carousel-image" />
                 </a>
               </div>
             </div>
