06d93e76e58b2568429df9db7073f8e919a0064c
Jeffery John <jefferyjohn2000@gmail.com>
2023-12-09 18:19:03 +0000

    add opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,39 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export const metadata: Metadata = {
+  metadataBase: new URL('https://jefferyjohn.com'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
   title: 'Jeffery John | Portfolio',
   description: 'Meet Jeffery John - a developer and designer @ CMU!',
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://jefferyjohn.com',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: '../public/og-image.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '../public/og-image.png',
+        width: 1800,
+        height: 1600,
+        alt: 'Jeffery John | Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
 }
 
 export default function RootLayout({
